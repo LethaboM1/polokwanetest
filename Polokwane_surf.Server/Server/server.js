d8a77e79d8faa40932abb155868dec24780c927d
LethaboM1 <developer@hillary.co.za>
2025-07-29 08:23:07 +0200

    changes

--- Polokwane_surf.Server/Server/server.js
+++ Polokwane_surf.Server/Server/server.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const PORT = process.env.PORT || 7059
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+const PORT = process.env.PORT || 7059;
+
+const corsOptions = {
+    origin: ['https://polokwanewebsite.netlify.app', 'http://localhost:5173'],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!' })
-})
+    res.json({ message: 'Hello from backend!' });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
